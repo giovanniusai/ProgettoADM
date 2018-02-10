a58e1d7dce79fe54729af9967f150d359ee7201a
giovanniusai <jannick.1993@yahoo.it>
2018-02-10 11:01:06 +0100

    Controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,4 +1,61 @@
-package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class Controller {
+
+    //Dichiarazione oggetti layout
+    @FXML
+    public Button buttonQuery1;
+
+    @FXML
+    public Button buttonQuery2;
+
+    @FXML
+    public Button buttonQuery3;
+
+    @FXML
+    public Button buttonQuery4;
+
+    @FXML
+    public Button buttonQuery5;
+
+    @FXML
+    public Button buttonQuery6;
+
+
+
+
+    //Metodi click sui bottoni
+    @FXML
+    public void buttonQuery1Clicked(){
+
+    }
+
+    @FXML
+    public void buttonQuery2Clicked(){
+
+    }
+
+    @FXML
+    public void buttonQuery3Clicked(){
+
+    }
+
+    @FXML
+    public void buttonQuery4Clicked(){
+
+    }
+
+    @FXML
+    public void buttonQuery5Clicked(){
+
+    }
+
+    @FXML
+    public void buttonQuery6Clicked(){
+
+    }
+
+
 }
+
